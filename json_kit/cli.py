a097e527d015133d5500eb34a6f70d5644133f9d
Tyler Fisher <tylerfisher@tylerfisher.ca>
2023-10-12 18:25:07 -0400

    Add support for translating JSON documents into DOT files

--- json_kit/cli.py
+++ json_kit/cli.py
@@ -2,7 +2,7 @@ import json
 import logging
 from typing import Iterable, Optional
 from json_kit.constants import JSON_INDENT
-import json_kit.json_schema as json_schema
+import json_kit.converter as converter
 import json_kit.files as files
 import click
 
@@ -22,14 +22,28 @@ def json_to_json_schema(input_files: Iterable[str], output_file: Optional[str]):
     JSON[L] -> JSON Schema
     """
     input_files = tuple(files.find(input_files, files_only=True))
-    schema = json_schema.generate_json_schema_from_files(input_files)
-    
+    schema = converter.generate_json_schema_from_files(input_files)
     blob = json.dumps(schema, indent=JSON_INDENT)
     if output_file:
         with open(output_file, 'w') as fp:
             fp.write(blob)
     else:
         print(blob)
+
+
+@main.command('json-to-dot')
+@click.argument('input-files', nargs=1, required=True)
+@click.option('--output-file', '-o', required=False)
+def json_to_dot(input_files: Iterable[str], output_file: Optional[str]):
+    input_files = tuple(files.find(input_files, files_only=True))
+    schema = converter.generate_json_schema_from_files(input_files)
+    dot = converter.json_schema_to_dot(schema)
+    if output_file:
+        with open(output_file, 'w') as fp:
+            fp.write(dot)
+    else:
+        print(dot)
+
 
 
 if __name__ == "__main__":
---
+++ json_kit/converter.py
@@ -0,0 +1,161 @@
+from networkx import DiGraph
+import json
+import sys
+import collections
+import time
+import genson
+from typing import Any, Iterable, Iterator, List, Optional, Set, Union
+
+import networkx.drawing.nx_pydot
+import logging
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_INDENT = 4
+
+
+# TODO
+def generate_json_schema(
+        docs: Iterable[Any], 
+        json_encoder_cls: Optional[json.JSONEncoder] = None) -> dict:
+    """
+    Generate a JSON schema from one or more JSON documents.
+
+    :param docs: one or more dictionaries or JSON documents
+    :return: a JSON schema
+    """
+    builder = genson.SchemaBuilder()
+    for doc in docs:
+        builder.add_object(doc)
+    return builder.to_schema()
+
+
+def generate_json_schema_from_files(
+        paths: Iterable[str],
+        json_encoder_cls: Optional[json.JSONEncoder] = None) -> dict:    
+    
+    schemas = []
+    for path in paths:
+        docs = _read_json_documents(path)
+        schema = generate_json_schema(docs, json_encoder_cls=json_encoder_cls)
+        if schema not in schemas:
+            schemas.append(schema)
+    return merge_json_schemas(schemas)
+
+
+def _read_json_documents(path: str) -> Iterator[Any]:
+    if path.endswith('.json'):
+        with open(path) as file:
+            doc = json.load(file)
+        yield doc
+
+    elif path.endswith('.jsonl'):
+        with open(path) as file:
+            lines = filter(bool, map(lambda line: line.strip(), file))
+            yield from map(json.loads, lines)
+    else:
+        raise ValueError(f"Unsupported file extension: {path}")
+
+
+def merge_json_schemas(schemas: Iterable[dict]) -> dict:
+    """
+    Merge one or more JSON schemas into a single JSON schema.
+
+    :param schemas: one or more JSON schemas
+    :return: a JSON schema
+    """
+    schemas = tuple(schemas)
+    n = len(schemas)
+    if n == 0:
+        raise ValueError("No JSON schemas provided")
+    elif n == 1:
+        return next(iter(schemas))
+
+    logger.info(f"Merging {n} JSON schemas...")
+    start_time = time.time()
+
+    builder = genson.SchemaBuilder()
+    for schema in schemas:
+        builder.add_schema(schema)
+    schema = builder.to_schema()
+
+    duration = time.time() - start_time
+    logger.info(f"Merged {n} JSON schemas in %.2f seconds", duration)
+    return schema
+        
+
+def json_schema_to_dot(schema: dict) -> str:
+    """
+    Convert a JSON schema to a DOT graph.
+
+    :param schema: a JSON schema
+    :return: a DOT graph
+    """
+    g = json_schema_to_nx_digraph(schema)
+    
+    # Convert the graph to DOT format.
+    d = networkx.drawing.nx_pydot.to_pydot(g)
+    d.set_rankdir('LR')
+
+    # Set the node and edge styles.
+    for node in d.get_nodes():
+        node.set_shape('box')
+        node.set_style('rounded')
+    
+    return d.to_string()
+
+
+def json_schema_to_nx_digraph(schema: dict) -> DiGraph:
+    keys = sorted(iter_keys_from_json_schema(schema))
+    
+    g = DiGraph()
+    for key in keys:
+        if '.' not in key:
+            g.add_node(key)
+            g.add_edge('.', key)
+        else:
+            parts = key.split('.')
+
+            # Add nodes for each part of the key
+            for i in range(len(parts)):
+                node = '.'.join(parts[:i + 1])
+                if node not in g.nodes:
+                    label = parts[i]
+                    g.add_node(node, label=label)
+
+            # Add edges between each part of the key
+            for i in range(len(parts) - 1):
+                source = '.'.join(parts[:i + 1])
+                target = '.'.join(parts[:i + 2])
+                if not g.has_edge(source, target):
+                    g.add_edge(source, target)
+
+    logger.info(f"Generated graph with {len(g.nodes)} nodes and {len(g.edges)} edges")
+    return g
+
+
+def iter_keys_from_json_schema(schema: dict) -> Iterator[str]:
+    def generator(o: Any, parent_keys: Optional[List[str]] = None, keys: Optional[Set[str]] = None) -> Iterator[str]:
+        keys = keys or set()
+        parent_keys = parent_keys or []
+
+        for key, value in o['properties'].items():
+            value_type = value['type']
+            if value_type == 'array':
+                key = f'{key}[]'
+                keys.add('.'.join(parent_keys + [key]))
+                subtype = value['items']['type']
+                if subtype == 'object':
+                    yield from generator(value['items'], parent_keys + [key], keys)
+
+            elif value_type == 'object':
+                keys.add('.'.join(parent_keys + [key]))
+                yield from generator(value, parent_keys + [key], keys)
+            else:
+                keys.add('.'.join(parent_keys + [key]))
+
+        yield from keys
+
+    yield from sorted(set(generator(schema)))
+
+    
